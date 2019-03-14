5f06b49798f62ed858a688905a20e76e1f465347
Ryan Delbango <Ryan.Delbango@gmail.com>
2019-03-13 21:37:12 -0400

    working messages

--- server/models/message.js
+++ server/models/message.js
@@ -8,14 +8,14 @@ const model = {
     
     },
     get(id, cb){
-        conn.query("SELECT id, firstName, lastName FROM FT_Users WHERE id=?", id, (err, data) => {
+        conn.query("SELECT idTo, firstName, lastName, message FROM FT_Messages join FT_Users on FT_Messages.idTo = FT_Users.id WHERE FT_Messages.id=?", id, (err, data) => {
             cb(err, data);  
         })
     
     },
 
     search(input, cb){
-        conn.query("SELECT firstName, lastName FROM FT_Users WHERE lastName = ? ", input.lastName, (err, data) => {
+        conn.query("SELECT id, idTo, message FROM FT_Messages WHERE message LIKE '%' ? '%' ", input.message, (err, data) => {
         cb(err, data);
         })
     
