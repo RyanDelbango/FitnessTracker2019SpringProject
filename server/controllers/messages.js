9f3b3d96b56cc461b7cca7d7e3b2c0fcf54f0326
Ryan Delbango <Ryan.Delbango@gmail.com>
2019-04-16 21:44:13 -0400

    async messages

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -4,52 +4,38 @@ const user = require('../models/message');
 const app = express.Router();
 
 // Get all messages by all users
-app.get("/", (req, res) => {
-
-    user.getAll((err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/", async (req, res, next) => {
+    user.getAll()
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Get all messages a user sent
-app.get("/:userid", (req, res) => {
-
-    user.get(req.params.userid, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/:userid", (req, res, next) => {
+    user.get(req.params.userid)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Get all messages from friends
-app.get("/friends/:userid", (req, res) => {
-
-    user.friends(req.params.userid, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/friends/:userid", (req, res, next) => {
+    user.friends(req.params.userid)
+        .then(x=>  res.send(x) )
+        .catch(next)
 });
 
 // Search all messages
-app.post("/search", (req, res) => {
-
-    user.search(req.body, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.post("/search", (req, res, next) => {
+    user.search(req.body)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Send a message
-app.post("/sendmessage/:userid/:idTo", (req, res) => {
-    user.create(req.params.userid, req.params.idTo, req.body, (err, data) => {
-        if (err) throw (err)
-        res.send(data);
-    });
-
+app.post("/sendmessage/:userid/:idTo", (req, res, next) => {
+    user.create(req.params.userid, req.params.idTo, req.body)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 
--- server/models/message.js
+++ server/models/message.js
@@ -1,46 +1,28 @@
 const conn = require('./mysql_connection');
 
-const model = {
-    getAll(cb){
-        conn.query("SELECT id, idTo, message FROM FT_Messages", (err, data) => {
-            cb(err, data);  
-        })
-    
+ const model = {
+    async getAll(){
+        return await conn.query("SELECT id, idTo, message FROM FT_Messages");
     },
     
-    get(id, cb){
-        conn.query("SELECT idTo, firstName, lastName, message FROM FT_Messages join FT_Users on FT_Messages.idTo = FT_Users.id WHERE FT_Messages.id=?", id, (err, data) => {
-            cb(err, data);  
-        })
-    
+    async get(id){
+        return await conn.query("SELECT idTo, firstName, lastName, message FROM FT_Messages join FT_Users on FT_Messages.idTo = FT_Users.id WHERE FT_Messages.id=?", id);
     },
 
-    friends(id, cb){
-        conn.query("SELECT FT_Friends.friend_id, firstName, lastName, message FROM FT_Users JOIN FT_Messages on FT_Users.id = FT_Messages.id JOIN FT_Friends on FT_Users.id = FT_Friends.friend_id WHERE FT_Friends.id = ?", id, (err, data) => {
-            cb(err, data); 
-        })
-    
+    async friends(id){
+        return await conn.query("SELECT FT_Friends.friend_id, firstName, lastName, message FROM FT_Users JOIN FT_Messages on FT_Users.id = FT_Messages.id JOIN FT_Friends on FT_Users.id = FT_Friends.friend_id WHERE FT_Friends.id = ?", id);
     },
 
-    search(input, cb){
-        conn.query("SELECT id, idTo, message FROM FT_Messages WHERE message LIKE '%' ? '%' ", input.message, (err, data) => {
-        cb(err, data);
-        })
-    
+    async search(input){
+        const data = conn.query("SELECT id, idTo, message FROM FT_Messages WHERE message LIKE '%' ? '%' ", input.message);
+        return data;
     },
 
     
-    create(userid, idTo, input, cb){
-        conn.query( "INSERT INTO FT_Messages (id, idTo, message) VALUES (?)",
-                    [[userid, idTo, input.message]],
-                    (err, data) => {
-                        if (err) {
-                            cb(err);
-                            return;
-                        }
-                         cb(err,data);
-                     }
-        )
+    async create(userid, idTo, input, cb){
+        const data = conn.query( "INSERT INTO FT_Messages (id, idTo, message) VALUES (?)",
+                    [[userid, idTo, input.message]]);
+                    return data;   
     }
 };
 
