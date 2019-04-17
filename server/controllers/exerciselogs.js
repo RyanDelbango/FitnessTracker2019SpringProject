c4520ea841fce86b1b595cc00ab2021333089cb0
Ryan Delbango <Ryan.Delbango@gmail.com>
2019-04-16 22:48:05 -0400

    async exercise logs

--- server/controllers/exerciselogs.js
+++ server/controllers/exerciselogs.js
@@ -4,91 +4,66 @@ const user = require('../models/exerciselog')
 const app = express.Router();
 
 // Get all exercise logs from all users
-app.get("/", (req, res) => {
-
-    user.getAll((err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/", (req, res, next) => {
+    user.getAll()
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Get exercise logs of a particular user
-app.get("/:userid", (req, res) => {
-
-    user.get(req.params.userid, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/:userid", (req, res, next) => {
+    user.get(req.params.userid)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Get a particular exercise log
-app.get("/logs/:log_id", (req, res) => {
-
-    user.getLog(req.params.log_id, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/logs/:log_id", (req, res, next) => {
+    user.getLog(req.params.log_id)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Get exercise logs of friends
-app.get("/friends/:userid", (req, res) => {
-
-    user.friends(req.params.userid, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/friends/:userid", (req, res, next) => {
+    user.friends(req.params.userid)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Get average weight monthly weight of a user
-app.get("/averageWeight/:userid", (req, res) => {
-
-    user.averageW(req.params.userid, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/averageWeight/:userid", (req, res, next) => {
+    user.averageW(req.params.userid)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Get average monthly time working out of a user
-app.get("/averageTime/:userid", (req, res) => {
-
-    user.averageT(req.params.userid, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/averageTime/:userid", (req, res, next) => {
+    user.averageT(req.params.userid)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Search exercise logs for a particular type of exercise
-app.post("/searchType", (req, res) => {
-
-    user.searchType(req.body, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.post("/searchType", (req, res, next) => {
+    user.searchType(req.body)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Create an exercise log
-app.post("/create/:userid", (req, res) => {
-    user.create(req.params.userid, req.body, (err, data) => {
-        if(err) throw (err);
-        res.send(data);
-    });
-
+app.post("/create/:userid", (req, res, next) => {
+    user.create(req.params.userid, req.body)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Edit an exercise log
-app.post("/edit/:userid/:logid", (req, res) => {
-    user.edit(req.params.userid, req.params.logid, req.body, (err, data) => {
-        if(err) throw (err);
-        res.send(data);
-    });
-
+app.post("/edit/:userid/:logid", (req, res, next) => {
+    user.edit(req.params.userid, req.params.logid, req.body)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 
--- server/models/exerciselog.js
+++ server/models/exerciselog.js
@@ -1,79 +1,42 @@
 const conn = require('./mysql_connection');
 
 const model = {
-    getAll(cb){
-        conn.query("SELECT log_id, FT_Users.id, firstName, lastName, day, month, year, weight, type, minutes, notes FROM FT_Users JOIN FT_ExerciseLogs on FT_Users.id = FT_ExerciseLogs.id", (err, data) => {
-            cb(err, data);  
-        })
-    
+    async getAll(){
+        return await conn.query("SELECT log_id, FT_Users.id, firstName, lastName, day, month, year, weight, type, minutes, notes FROM FT_Users JOIN FT_ExerciseLogs on FT_Users.id = FT_ExerciseLogs.id");
     },
     
-    get(id, cb){
-        conn.query("SELECT log_id, FT_Users.id, firstName, lastName, day, month, year, weight, type, minutes, notes FROM FT_Users JOIN FT_ExerciseLogs on FT_Users.id = FT_ExerciseLogs.id WHERE FT_Users.id = ?", id, (err, data) => {
-            cb(err, data); 
-        })
-    
+    async get(id){
+        return await conn.query("SELECT log_id, FT_Users.id, firstName, lastName, day, month, year, weight, type, minutes, notes FROM FT_Users JOIN FT_ExerciseLogs on FT_Users.id = FT_ExerciseLogs.id WHERE FT_Users.id = ?", id);
     },
 
-    getLog(log_id, cb){
-        conn.query("SELECT FT_ExerciseLogs.log_id, FT_Users.id, firstName, lastName, day, month, year, weight, type, minutes, notes, COUNT(comment) AS 'Number of Comments' FROM FT_Users JOIN FT_ExerciseLogs on FT_Users.id = FT_ExerciseLogs.id JOIN FT_ExerciseComments on FT_ExerciseLogs.log_id = FT_ExerciseComments.log_id WHERE FT_ExerciseLogs.log_id = ?", log_id, (err, data) => {
-            cb(err, data); 
-        })
-    
+    async getLog(log_id){
+        return await conn.query("SELECT FT_ExerciseLogs.log_id, FT_Users.id, firstName, lastName, day, month, year, weight, type, minutes, notes, COUNT(comment) AS 'Number of Comments' FROM FT_Users JOIN FT_ExerciseLogs on FT_Users.id = FT_ExerciseLogs.id JOIN FT_ExerciseComments on FT_ExerciseLogs.log_id = FT_ExerciseComments.log_id WHERE FT_ExerciseLogs.log_id = ?", log_id);
     },
 
-    friends(id, cb){
-        conn.query("SELECT log_id, FT_Friends.friend_id, firstName, lastName, day, month, year, weight, type, minutes, notes FROM FT_Users JOIN FT_ExerciseLogs on FT_Users.id = FT_ExerciseLogs.id JOIN FT_Friends on FT_Users.id = FT_Friends.friend_id WHERE FT_Friends.id = ?", id, (err, data) => {
-            cb(err, data); 
-        })
-    
+    async friends(id){
+        return await conn.query("SELECT log_id, FT_Friends.friend_id, firstName, lastName, day, month, year, weight, type, minutes, notes FROM FT_Users JOIN FT_ExerciseLogs on FT_Users.id = FT_ExerciseLogs.id JOIN FT_Friends on FT_Users.id = FT_Friends.friend_id WHERE FT_Friends.id = ?", id);
     },
 
-    searchType(input, cb){
-        conn.query("SELECT log_id, FT_Users.id, firstName, lastName, type, day, month, year FROM FT_ExerciseLogs join FT_Users on FT_ExerciseLogs.id = FT_Users.id WHERE type = ? ", input.type, (err, data) => {
-        cb(err, data);
-        })
-    
+    async searchType(input){
+        return await conn.query("SELECT log_id, FT_Users.id, firstName, lastName, type, day, month, year FROM FT_ExerciseLogs join FT_Users on FT_ExerciseLogs.id = FT_Users.id WHERE type = ? ", input.type);
     },
 
-    averageW(id, cb){
-        conn.query("SELECT month, year, AVG(weight) AS 'Average weight' FROM FT_ExerciseLogs WHERE id=? GROUP BY month, year", id, (err, data) => {
-        cb(err, data);
-        })
-    
+    async averageW(id){
+        return await conn.query("SELECT month, year, AVG(weight) AS 'Average weight' FROM FT_ExerciseLogs WHERE id=? GROUP BY month, year", id);
     },
 
-    averageT(id, cb){
-        conn.query("SELECT month, year, AVG(minutes) AS 'Average time exercising in minutes' FROM FT_ExerciseLogs WHERE id=? GROUP BY month, year", id, (err, data) => {
-        cb(err, data);
-        })
-    
+    async averageT(id){
+        return await conn.query("SELECT month, year, AVG(minutes) AS 'Average time exercising in minutes' FROM FT_ExerciseLogs WHERE id=? GROUP BY month, year", id);
     },
     
-    create(userid, input, cb){
-        conn.query("INSERT INTO FT_ExerciseLogs (id, day, month, year, weight, minutes, type, notes) VALUES (?)",
-                    [[userid, input.day, input.month, input.year, input.weight, input.minutes, input.type, input.notes]],
-                    (err, data) => {
-                        if (err) {
-                            cb(err);
-                            return;
-                        }
-                         cb(err,data);
-                     }
-        )
+    async create(userid, input){
+        return await conn.query("INSERT INTO FT_ExerciseLogs (id, day, month, year, weight, minutes, type, notes) VALUES (?)",
+                    [[userid, input.day, input.month, input.year, input.weight, input.minutes, input.type, input.notes]]);
     },
 
-    edit(userid, logid, input, cb){
-        conn.query("REPLACE INTO FT_ExerciseLogs (id, log_id, day, month, year, weight, minutes, type, notes) VALUES (?)",
-                    [[userid, logid, input.day, input.month, input.year, input.weight, input.minutes, input.type, input.notes]],
-                    (err, data) => {
-                        if (err) {
-                            cb(err);
-                            return;
-                        }
-                         cb(err,data);
-                     }
-        )
+    async edit(userid, logid, input){
+        return await conn.query("REPLACE INTO FT_ExerciseLogs (id, log_id, day, month, year, weight, minutes, type, notes) VALUES (?)",
+                    [[userid, logid, input.day, input.month, input.year, input.weight, input.minutes, input.type, input.notes]]);
     }
     
 };
