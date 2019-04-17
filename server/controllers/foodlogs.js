8067edd88cf3764110d14646ce79fb5e8b7d6326
Ryan Delbango <Ryan.Delbango@gmail.com>
2019-04-16 22:26:49 -0400

    async food logs

--- server/controllers/foodlogs.js
+++ server/controllers/foodlogs.js
@@ -4,81 +4,59 @@ const user = require('../models/foodlog');
 const app = express.Router();
 
 // Get all food Logs of all Users
-app.get("/", (req, res) => {
-
-    user.getAll((err, data) => {
-        if(err) throw err;
-        res.send(data);
+app.get("/", (req, res, next) => {
+    user.getAll()
+        .then(x=>  res.send(x) )
+        .catch(next)
     });
-
-});
 
 // Get food logs of a particular user
-app.get("/:userid", (req, res) => {
-
-    user.get(req.params.userid, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/:userid", (req, res, next) => {
+    user.get(req.params.userid)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Get a particular food log
-app.get("/logs/:log_id", (req, res) => {
-
-    user.getLog(req.params.log_id, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/logs/:log_id", (req, res, next) => {
+    user.getLog(req.params.log_id)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Get exercise logs of friends
-app.get("/friends/:userid", (req, res) => {
-
-    user.friends(req.params.userid, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/friends/:userid", (req, res, next) => {
+    user.friends(req.params.userid)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Total daily calories consumbed by a user
-app.get("/totalcalories/:userid", (req, res) => {
-
-    user.total(req.params.userid, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/totalcalories/:userid", (req, res, next) => {
+    user.total(req.params.userid)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Average monthly calories by user
-app.get("/averagecalories/:userid", (req, res) => {
-
-    user.average(req.params.userid, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/averagecalories/:userid", (req, res, next) => {
+    user.average(req.params.userid)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Create a food log
-app.post("/create/:userid", (req, res) => {
-    user.create(req.params.userid, req.body, (err, data) => {
-        if(err) throw (err)
-        res.send(data);
-    });
-
+app.post("/create/:userid", (req, res, next) => {
+    user.create(req.params.userid, req.body)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Edit an existing food log
-app.post("/edit/:userid/:logid", (req, res) => {
-    user.edit(req.params.userid, req.params.logid, req.body, (err, data) => {
-        if(err) throw (err);
-        res.send(data);
-    });
-
+app.post("/edit/:userid/:logid", (req, res, next) => {
+    user.edit(req.params.userid, req.params.logid, req.body)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 
--- server/models/foodlog.js
+++ server/models/foodlog.js
@@ -1,74 +1,39 @@
 const conn = require('./mysql_connection');
 
 const model = {
-    getAll(cb){
-        conn.query("SELECT log_id, FT_Users.id, firstName, lastName, day, month, year, bcalories, lcalories, dcalories, scalories, breakfast, lunch, dinner, snacks FROM FT_Users JOIN FT_FoodLogs on FT_Users.id = FT_FoodLogs.id ORDER BY log_id", (err, data) => {
-            cb(err, data);  
-        })
-    
+    async getAll(){
+        return await conn.query("SELECT log_id, FT_Users.id, firstName, lastName, day, month, year, bcalories, lcalories, dcalories, scalories, breakfast, lunch, dinner, snacks FROM FT_Users JOIN FT_FoodLogs on FT_Users.id = FT_FoodLogs.id ORDER BY log_id");
     },
 
-    get(id, cb){
-        conn.query("SELECT firstName, lastName, day, month, year, bcalories, lcalories, dcalories, scalories, breakfast, lunch, dinner, snacks FROM FT_Users JOIN FT_FoodLogs on FT_Users.id = FT_FoodLogs.id WHERE FT_Users.id = ? ORDER BY log_id DESC", id, (err, data) => {
-            cb(err, data); 
-        })
-    
+    async get(id){
+        return await conn.query("SELECT firstName, lastName, day, month, year, bcalories, lcalories, dcalories, scalories, breakfast, lunch, dinner, snacks FROM FT_Users JOIN FT_FoodLogs on FT_Users.id = FT_FoodLogs.id WHERE FT_Users.id = ? ORDER BY log_id DESC", id);
     },
 
-    getLog(log_id, cb){
-        conn.query("SELECT FT_FoodLogs.log_id, firstName, lastName, day, month, year, bcalories, lcalories, dcalories, scalories, breakfast, lunch, dinner, snacks, COUNT(comment) AS 'Number of Comments' FROM FT_Users JOIN FT_FoodLogs on FT_Users.id = FT_FoodLogs.id JOIN FT_FoodComments on FT_FoodLogs.log_id = FT_FoodComments.log_id WHERE FT_FoodLogs.log_id = ?", log_id, (err, data) => {
-            cb(err, data); 
-        })
-    
+    async getLog(log_id){
+        return await conn.query("SELECT FT_FoodLogs.log_id, firstName, lastName, day, month, year, bcalories, lcalories, dcalories, scalories, breakfast, lunch, dinner, snacks, COUNT(comment) AS 'Number of Comments' FROM FT_Users JOIN FT_FoodLogs on FT_Users.id = FT_FoodLogs.id JOIN FT_FoodComments on FT_FoodLogs.log_id = FT_FoodComments.log_id WHERE FT_FoodLogs.log_id = ?", log_id);
     },
 
-    friends(id, cb){
-        conn.query("SELECT FT_Friends.friend_id, firstName, lastName, day, month, year, bcalories, lcalories, dcalories, scalories, breakfast, lunch, dinner, snacks FROM FT_Users JOIN FT_FoodLogs on FT_Users.id = FT_FoodLogs.id JOIN FT_Friends on FT_Friends.friend_id = FT_Users.id WHERE FT_Friends.id = ? ORDER BY log_id DESC", id, (err, data) => {
-            cb(err, data); 
-        })
-    
+    async friends(id){
+        return await conn.query("SELECT FT_Friends.friend_id, firstName, lastName, day, month, year, bcalories, lcalories, dcalories, scalories, breakfast, lunch, dinner, snacks FROM FT_Users JOIN FT_FoodLogs on FT_Users.id = FT_FoodLogs.id JOIN FT_Friends on FT_Friends.friend_id = FT_Users.id WHERE FT_Friends.id = ? ORDER BY log_id DESC", id);
     },
 
-    total(id, cb){
-        conn.query("SELECT day, month, year, SUM(bcalories + lcalories + dcalories) AS 'Total Calories' FROM FT_FoodLogs WHERE id = ? GROUP BY day, month, year", id, (err, data) => {
-        cb(err, data);
-        })
-    
+    async total(id){
+        return await conn.query("SELECT day, month, year, SUM(bcalories + lcalories + dcalories) AS 'Total Calories' FROM FT_FoodLogs WHERE id = ? GROUP BY day, month, year", id);
     },
 
-    average(id, cb){
-        conn.query("SELECT month, year, AVG(bcalories + lcalories + dcalories) AS 'Average Calories Per Month' FROM FT_FoodLogs WHERE id = ? GROUP BY month, year", id, (err, data) => {
-        cb(err, data);
-        })
-    
+    async average(id){
+        return await conn.query("SELECT month, year, AVG(bcalories + lcalories + dcalories) AS 'Average Calories Per Month' FROM FT_FoodLogs WHERE id = ? GROUP BY month, year", id);
     },
     
-    create(userid, input, cb){
-        conn.query("INSERT INTO FT_FoodLogs (id, day, month, year, bcalories, lcalories, dcalories, scalories, breakfast, lunch, dinner, snacks) VALUES (?)",
-                    [[userid, input.day, input.month, input.year, input.bcalories, input.lcalories, input.dcalories, input.scalories, input.breakfast, input.lunch, input.dinner, input.snacks]],
-                    (err, data) => {
-                        if (err) {
-                            cb(err);
-                            return;
-                        }
-                         cb(err,data);
-                     }
-        )
+    async create(userid, input){
+        return await conn.query("INSERT INTO FT_FoodLogs (id, day, month, year, bcalories, lcalories, dcalories, scalories, breakfast, lunch, dinner, snacks) VALUES (?)",
+                    [[userid, input.day, input.month, input.year, input.bcalories, input.lcalories, input.dcalories, input.scalories, input.breakfast, input.lunch, input.dinner, input.snacks]])
     },
 
-    edit(userid, logid, input, cb){
-        conn.query("REPLACE INTO FT_FoodLogs (id, log_id, day, month, year, bcalories, lcalories, dcalories, scalories, breakfast, lunch, dinner, snacks) VALUES (?)",
-        [[userid, logid, input.day, input.month, input.year, input.bcalories, input.lcalories, input.dcalories, input.scalories, input.breakfast, input.lunch, input.dinner, input.snacks]],
-                    (err, data) => {
-                        if (err) {
-                            cb(err);
-                            return;
-                        }
-                         cb(err,data);
-                     }
-        )
+    async edit(userid, logid, input){
+        return await conn.query("REPLACE INTO FT_FoodLogs (id, log_id, day, month, year, bcalories, lcalories, dcalories, scalories, breakfast, lunch, dinner, snacks) VALUES (?)",
+        [[userid, logid, input.day, input.month, input.year, input.bcalories, input.lcalories, input.dcalories, input.scalories, input.breakfast, input.lunch, input.dinner, input.snacks]]);
     }
-    
 };
 
 module.exports = model;
