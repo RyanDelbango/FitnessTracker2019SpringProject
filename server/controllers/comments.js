3c346d1c2ba322dac96e34e578b2d9db8029492b
Ryan Delbango <Ryan.Delbango@gmail.com>
2019-04-16 23:14:15 -0400

    async comments

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -4,61 +4,45 @@ const user = require('../models/comment');
 const app = express.Router();
 
 // Get all comments on food logs
-app.get("/food", (req, res) => {
-
-    user.getAllF((err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/food", (req, res, next) => {
+    user.getAllF()
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Get all comments on a particular food log
-app.get("/food/:log_id", (req, res) => {
-
-    user.getF(req.params.log_id, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/food/:log_id", (req, res, next) => {
+    user.getF(req.params.log_id)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Get all comments on a particular exercise log
-app.get("/exercise/:log_id", (req, res) => {
-
-    user.getE(req.params.log_id, (err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/exercise/:log_id", (req, res, next) => {
+    user.getE(req.params.log_id)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Get all comments on exercise logs
-app.get("/exercise", (req, res) => {
-
-    user.getAllE((err, data) => {
-        if(err) throw err;
-        res.send(data);
-    });
-
+app.get("/exercise", (req, res, next) => {
+    user.getAllE()
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Create comments on exercise logs
-app.post("/exercise/:userid/:log_id", (req, res) => {
-    user.createE(req.params.userid, req.params.log_id, req.body, (err, data) => {
-        if (err) throw (err)
-        res.send(data);
-    });
-
+app.post("/exercise/:userid/:log_id", (req, res, next) => {
+    user.createE(req.params.userid, req.params.log_id, req.body)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 // Create comments on food logs
-app.post("/food/:userid/:log_id", (req, res) => {
-    user.createF(req.params.userid, req.params.log_id, req.body, (err, data) => {
-        if (err) throw (err)
-        res.send(data);
-    });
-
+app.post("/food/:userid/:log_id", (req, res, next) => {
+    user.createF(req.params.userid, req.params.log_id, req.body)
+    .then(x=>  res.send(x) )
+    .catch(next)
 });
 
 
--- server/models/comment.js
+++ server/models/comment.js
@@ -2,58 +2,30 @@ const conn = require('./mysql_connection');
 
 const model = {
 
-    getAllE(cb){
-        conn.query("SELECT log_id, firstName, lastName, comment FROM FT_ExerciseComments JOIN FT_Users on FT_ExerciseComments.id = FT_Users.id", (err, data) => {
-            cb(err, data);  
-        })
-    
+    async getAllE(){
+        return await conn.query("SELECT log_id, firstName, lastName, comment FROM FT_ExerciseComments JOIN FT_Users on FT_ExerciseComments.id = FT_Users.id");
     },
 
-    getE(log_id, cb){
-        conn.query("SELECT log_id, firstName, lastName, comment FROM FT_ExerciseComments JOIN FT_Users on FT_ExerciseComments.id = FT_Users.id WHERE log_id = ?", log_id, (err, data) => {
-            cb(err, data);  
-        })
-    
+    async getE(log_id){
+        return await conn.query("SELECT log_id, firstName, lastName, comment FROM FT_ExerciseComments JOIN FT_Users on FT_ExerciseComments.id = FT_Users.id WHERE log_id = ?", log_id);
     },
 
-    getAllF(cb){
-        conn.query("SELECT log_id, firstName, lastName, comment FROM FT_FoodComments JOIN FT_Users on FT_FoodComments.id = FT_Users.id", (err, data) => {
-            cb(err, data);  
-        })
-    
+    async getAllF(){
+        return await conn.query("SELECT log_id, firstName, lastName, comment FROM FT_FoodComments JOIN FT_Users on FT_FoodComments.id = FT_Users.id");
     },
 
-    getF(log_id, cb){
-        conn.query("SELECT log_id, firstName, lastName, comment FROM FT_FoodComments JOIN FT_Users on FT_FoodComments.id = FT_Users.id WHERE log_id = ?", log_id, (err, data) => {
-            cb(err, data);  
-        })
-    
+    async getF(log_id){
+        return await conn.query("SELECT log_id, firstName, lastName, comment FROM FT_FoodComments JOIN FT_Users on FT_FoodComments.id = FT_Users.id WHERE log_id = ?", log_id);
     },
 
-    createE(userid, log_id, input, cb){
-        conn.query("INSERT INTO FT_ExerciseComments (id, log_id, comment) VALUES (?)",
-                    [[userid, log_id, input.comment]],
-                    (err, data) => {
-                        if (err) {
-                            cb(err);
-                            return;
-                        }
-                         cb(err,data);
-                     }
-        )
+    async createE(userid, log_id, input){
+        return await conn.query("INSERT INTO FT_ExerciseComments (id, log_id, comment) VALUES (?)",
+                    [[userid, log_id, input.comment]]);
     },
     
-    createF(userid, log_id, input, cb){
-        conn.query("INSERT INTO FT_FoodComments (id, log_id, comment) VALUES (?)",
-                    [[userid, log_id, input.comment]],
-                    (err, data) => {
-                        if (err) {
-                            cb(err);
-                            return;
-                        }
-                         cb(err,data);
-                     }
-        )
+    async createF(userid, log_id, input){
+        return await conn.query("INSERT INTO FT_FoodComments (id, log_id, comment) VALUES (?)",
+                    [[userid, log_id, input.comment]]);
     }
 };
 
